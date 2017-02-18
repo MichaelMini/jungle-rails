01e4f96a59266c0326cf36b13c1734c8cefb034a
MichaelMini <mcho911@gmail.com>
2017-02-18 05:01:34 +0000

    adding review feature

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,12 +2,13 @@ class ProductsController < ApplicationController
 
   def index
     @products = Product.all.order(created_at: :desc)
-    @reviews = Review.all.order(created_at: :desc)
+    @reviews = Review.all.order(created_at: :desc) # ???
   end
 
   def show
     @product = Product.find params[:id]
-    @user = User.find params[:id]
+    # @user = User.find params[:id]
+    @review =Review.new(:product=>@product) # ???
   end
 
 end
