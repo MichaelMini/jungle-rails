b28694df579c86d73a88692349b06564c55a3731
MichaelMini <mcho911@gmail.com>
2017-02-18 21:16:41 +0000

    added destroy route

--- config/routes.rb
+++ config/routes.rb
@@ -19,7 +19,7 @@ Rails.application.routes.draw do
   end
 
   resources :products do
-    resources :reviews, only: [:create]
+    resources :reviews, only: [:create, :destroy]
   end
 
   get '/login' => 'sessions#new'
