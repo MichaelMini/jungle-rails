d0dfb21c746c8d0f9f24e808a6bfa3fe7447bd88
MichaelMini <mcho911@gmail.com>
2017-02-18 01:04:00 +0000

    declared @user

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,10 +2,12 @@ class ProductsController < ApplicationController
 
   def index
     @products = Product.all.order(created_at: :desc)
+    @reviews = Review.all.order(created_at: :desc)
   end
 
   def show
     @product = Product.find params[:id]
+    @user = User.find params[:id]
   end
 
 end
