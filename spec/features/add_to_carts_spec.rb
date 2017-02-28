a205f82587a5b656fbf315a68b6140216c681d34
MichaelMini <mcho911@gmail.com>
2017-02-28 02:38:07 +0000

    added average rating

---
+++ spec/features/add_to_carts_spec.rb
@@ -0,0 +1,34 @@
+require 'rails_helper'
+
+RSpec.feature "AddToCarts", type: :feature, js: true do
+
+  # SETUP
+  before :each do
+    @category = Category.create! name: 'Apparel'
+
+    10.times do |n|
+      @category.products.create!(
+        name:  Faker::Hipster.sentence(3),
+        description: Faker::Hipster.paragraph(4),
+        image: open_asset('apparel1.jpg'),
+        quantity: 10,
+        price: 64.99
+      )
+    end
+  end
+
+  scenario "They see cart amount" do
+
+    visit root_path
+
+    first('article.product').find_link('Add').click
+    # ACT
+    # visit "/products/#{@category.products.first.id}"
+    expect(page).to have_content 'My Cart (1)'
+    # DEBUG / VERIFY
+    save_screenshot
+
+    # expect(page).to have_css 'article.product', count: 10
+  end
+
+end
